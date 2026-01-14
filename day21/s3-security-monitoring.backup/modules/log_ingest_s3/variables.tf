10ac0f243825c6537a55d2836af3906005cd650c
Ammarkhan96 <ammarkhaan96@gmail.com>
2026-01-14 13:50:27 +0500

    terraform-work

---
+++ day21/s3-security-monitoring.backup/modules/log_ingest_s3/variables.tf
@@ -0,0 +1,5 @@
+
+variable "monitored_bucket_name" {
+  description = "Base name for the bucket to monitor (random suffix will be added)"
+  type        = string
+}
---
+++ day21/s3-security-monitoring.backup/modules/security_alarm/outputs.tf
@@ -0,0 +1,8 @@
+
+output "denied_alarm_arn" {
+  value = aws_cloudwatch_metric_alarm.denied_requests_alarm.arn
+}
+
+output "restricted_alarm_arn" {
+  value = aws_cloudwatch_metric_alarm.restricted_prefix_alarm.arn
+}
